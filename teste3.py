152bac7dc00019bb591cce108d40216950c40cf2
rodrigolmiranda <rodrigolmiranda@gmail.com>
2020-09-11 23:38:10 +0800

    firt comit

---
+++ teste3.py
@@ -0,0 +1,14 @@
+
+import numpy as np
+import seaborn as sns
+import matplotlib.pyplot as plt
+
+x = np.linspace(0, 100, 100)
+y = np.linspace(0, 100, 100)
+sns.lineplot(x = x, y= y**2)
+plt.show()
+# plt.plot(x, np.sin(x))
+# plt.show()
+print("teste")
+
+print("teste2")
